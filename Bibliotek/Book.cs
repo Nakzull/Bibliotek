931e36d30e1b8ef9c83f5298c93fd21d12cef2a7
Nakzull <andr25f4@zbc.dk>
2021-08-31 12:34:02 +0200

    Add project files.

---
+++ Bibliotek/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bibliotek
+{
+    class Book
+    {
+        public string name { get; set; }
+        public int year { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public int version { get; set; }
+
+        public int stock { get; set; }
+
+        public Book(string name, string author, int year, string publisher, int version, int stock)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+            this.publisher = publisher;
+            this.version = version;
+            this.stock = stock;
+        }
+    }
+}
---
+++ Bibliotek/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bibliotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book harryPotter = new Book("Harry Potter", "J.K. Rowling", 2002, "Bloomsbury Publishing", 1, 5);
+            Book lordOfTheRings = new Book("Lord of the Rings", "J.R. Tolkien", 1960, "Allen & Unwin", 2 , 4);
+            Book eragon = new Book("Eragon", "Christopher Paolini", 2005, "Paolini", 1, 2);
+            Book redRising = new Book("Red Rising", "Pierce Brown", 2010, "Del Rey Books", 5 , 3);
+            List<Book> books = new List<Book>();
+            books.Add(harryPotter);
+            books.Add(lordOfTheRings);
+            books.Add(eragon);
+            books.Add(redRising);
+            Stack<Book> userBookCart = new Stack<Book>();
+            ReturnPoint:
+            Console.WriteLine("Which book do you wish to borrow? (1-5)\n1: Harry Potter - number in stock: {0}\n2: Lord of the Rings - number in stock: {1}\n3: Eragon - number in stock: {2}\n4: Red Rising - number in stock: {3}\n5: Checkout your books", harryPotter.stock, lordOfTheRings.stock, eragon.stock, redRising.stock);
+            int userWish = Convert.ToInt32(Console.ReadLine());
+            switch (userWish)
+            {
+                case 1:
+                    if (harryPotter.stock > 0)
+                    {
+                        userBookCart.Push(harryPotter);
+                        harryPotter.stock--;
+                    }
+                    else
+                        Console.WriteLine("That book is unfortunately out of stock");
+                    goto ReturnPoint;
+                case 2:
+                    if (lordOfTheRings.stock > 0)
+                    {
+                        userBookCart.Push(lordOfTheRings);
+                        lordOfTheRings.stock--;
+                    }
+                    else
+                        Console.WriteLine("That book is unfortunately out of stock");
+                    goto ReturnPoint;
+                case 3:
+                    if (eragon.stock > 0)
+                    {
+                        userBookCart.Push(eragon);
+                        eragon.stock--;
+                    }
+                    else
+                        Console.WriteLine("That book is unfortunately out of stock");
+                    goto ReturnPoint;
+                case 4:
+                    if (redRising.stock > 0)
+                    {
+                        userBookCart.Push(redRising);
+                        redRising.stock--;
+                    }
+                    else
+                        Console.WriteLine("That book is unfortunately out of stock");
+                    goto ReturnPoint;
+                case 5:
+                    foreach (var items in userBookCart)
+                    {                        
+                        Console.WriteLine("You selected {0}\nDo you still wish to borrow that book? (yes/no)", items.name);
+                        string userConfirm = Console.ReadLine();
+                        if (userConfirm == "no")
+                            Console.WriteLine("{0} has been removed from your cart", userBookCart.Peek());
+                        else
+                            Console.WriteLine("{0} has been checked out", userBookCart.Peek());
+                        userBookCart.Pop();
+                    }
+                    break;
+            }    
+        }
+    }
+}
